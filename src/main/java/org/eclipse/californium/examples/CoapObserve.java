6c6d12bbadfc997083328f2c3c3ab7171ccdff20
Wellington Vieira <wellingtonsv@gmail.com>
2019-04-27 14:37:14 -0300

    Ajustes no observador

--- src/main/java/org/eclipse/californium/examples/CoapObserve.java
+++ src/main/java/org/eclipse/californium/examples/CoapObserve.java
@@ -18,6 +18,12 @@ import org.eclipse.californium.core.coap.CoAP.Type;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 import org.eclipse.californium.core.server.resources.Resource;
 
+/**
+ * Classe Responsável por obsevar qualquer mudança na temperatura e umidade.
+ * 
+ * @author wvieira
+ *
+ */
 public class CoapObserve extends CoapResource {
 	private String temp;
 	private String humi;
@@ -32,25 +38,35 @@ public class CoapObserve extends CoapResource {
 
 	public CoapObserve(String name, HelloWorldServer server) {
 		super(name);
-		setObservable(true);
-		setObserveType(Type.CON);
-		getAttributes().setObservable();
-
+		setObservable(true); // permitir a observação.
+		setObserveType(Type.CON); // configurar o tipo de notificação para CONs.
+		getAttributes().setObservable(); // marca como observável no formato de link.
+		
+		// Agendador de tarefa de atualização periódica, verifica a mudança. Tempo definido em mls.
 		Timer timer = new Timer();
 		timer.schedule(new UpdateTask(), 0L, 60000L);
 
-		this.server = server;
-		this.resource = new HelloWorldResource();
-		this.server.add(new Resource[] { this.resource });
-		this.controlGpioExample = new ControlGpioExample();
+		this.server = server; //Seta o servidor já instanciado na Classe HelloWordServer.
+		this.resource = new HelloWorldResource(); // Inicializa o Classe Recurso.
+		this.server.add(new Resource[] { this.resource });// Adiciona o Recurso ao servidor Coap.
+		this.controlGpioExample = new ControlGpioExample();// Instancia a classe da GPIO.
 	}
-
+	
+	/**
+	 * Classe interna que verifica a mudança de estado na temperatura e umidade.
+	 * 
+	 * @author wvieira
+	 *
+	 */
 	private class UpdateTask extends TimerTask {
+		
 		private UpdateTask() {
 		}
-
+		
+		//Atualização periódica do recurso
 		public void run() {
 			try {
+				//Metodo interno para aferir a temperatura e umidade
 				CoapObserve.this.getResultsTempHumi();
 			} catch (IOException | ScriptException | InterruptedException e) {
 				e.printStackTrace();
@@ -69,24 +85,34 @@ public class CoapObserve extends CoapResource {
 	}
 
 	public void handlePUT(CoapExchange exchange) {
+		//Recupera do cliente Coap o valores para definir o valor padrão
+		//para acionamento dos LED's.(25&30) 
+		//25-Valor correspondente a temperatura
+		//30-Valor correspondente a umidade
 		String retrieve = exchange.getRequestText();
 		System.out.println(retrieve);
+		//Separação do valores...
 		String[] tempHumi = retrieve.split("&");
+		
 		if (null != tempHumi[0]) {
 			this.tempDefUser = tempHumi[0];
 		} else {
-			this.tempDefUser = "30.0";
+			this.tempDefUser = "30.0"; //Define o valor para padrão da temperatura
 		}
+		
 		System.out.println(this.tempDefUser);
+		
 		if (null != tempHumi[1]) {
 			this.humiDefUser = tempHumi[1];
 		} else {
-			this.humiDefUser = "70.0";
+			this.humiDefUser = "70.0"; //Define o valor para padrão da umidade
 		}
-		System.out.println(this.humiDefUser);
-		exchange.respond(ResponseCode.CHANGED);
-		changed();
 		
+		System.out.println(this.humiDefUser);
+		
+		/*
+		 * Logica de desligar o LED 1 (vermelho), se eles estão em estado de ligado.
+		 */
 		if (this.ledOne) {
 			System.out.println("Desligou o led 1...");
 			try {
@@ -97,6 +123,9 @@ public class CoapObserve extends CoapResource {
 			}
 		}
 		
+		/*
+		 * Logica de desligar o LED 2 (verde), se eles estão em estado de ligado.
+		 */
 		if (this.ledTwo) {
 			System.out.println("Desligou o led 2...");
 			try {
@@ -106,52 +135,80 @@ public class CoapObserve extends CoapResource {
 				e.printStackTrace();
 			}
 		}
-		exchange.respond(ResponseCode.CHANGED);
-		changed();
+		
+		exchange.respond(ResponseCode.CHANGED);//Adiciona a mudança para ser notificada
+		changed();//Notifica todos os observadores da mudança
 	}
 
+	/**
+	 * Metodo que verifica a tempetura e humidade, para saber se estão dentro dos padrões.
+	 * 
+	 */
 	private synchronized void getResultsTempHumi() throws IOException, ScriptException, InterruptedException {
 		System.out.println("Entrou no metodo de verificar a Temperatura e Umidade....");
-
+		
+		/*
+		 * Inicialização e verificação no Python, para acionar o sensor DHT
+		 * 1º parametro determina o tipo de modelo de sensor (11)
+		 * 2º parametro define a porta na GPIO (4)
+		 */
 		Process p = Runtime.getRuntime().exec("python /home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 11 4");
-
+		
+		/*
+		 * leia a saída do comando
+		 */
 		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
 		if (stdInput != null) {
-			System.out.println("Here is the standard output of the command:\n");
+			//Saída padrão do comando.
+			System.out.println("Aqui está a saída padrão do comando:\n");
 			Pattern pt = Pattern.compile("\\d+\\.\\d+");
 			Matcher m = null;
 			this.temp = "0.0";
 			this.humi = "0.0";
 			String answerTemp = null;
+			
 			while ((answer = stdInput.readLine()) != null) {
 				System.out.println("Resultado do Python: " + answer);
 				answerTemp = answer;
 			}
+			
 			List<String> resultRegex = new ArrayList<String>();
 			m = pt.matcher(answerTemp);
 			while (m.find()) {
 				resultRegex.add(m.group());
 			}
-			System.out.println("Tamanho Lista: " + resultRegex.size());
-			this.temp = ((String) resultRegex.get(0));
-			this.humi = ((String) resultRegex.get(1));
+			
+			//System.out.println("Tamanho Lista: " + resultRegex.size());
+			this.temp = resultRegex.get(0);
+			this.humi = resultRegex.get(1);
 
 			this.server.setAnswerfull("Temperatura: " + this.temp + "Cº" + "\tUmidade: " + this.humi + "%");
-			System.out.println("Obteve recurso: " + this.server.getAnswerfull());
-			this.resource.setAnswer(this.server.getAnswerfull());
+			//System.out.println("Obteve recurso: " + this.server.getAnswerfull());
+			this.resource.setAnswer(this.server.getAnswerfull()); //Adiciona a resposta no HelloWorldResource 
+			//para o HelloWorldServer Servido COAP.
+			
+			//Defensiva para não inicializar a GPIO novamente
+			//Evita um exeção de IllegalStateException
 			if (!this.controlGpioExample.isIniciou()) {
 				this.controlGpioExample.setup();
 			}
+			
+			
+			/*
+			 * Logica de verificar se a temperatura e humidade estão dentro do padrão 
+			 * inicial ou dentro do valores definidos pelo usuario, atraves do cliente Coap.
+			 */
 			System.out.println("Temperatura padrão: " + Float.parseFloat(this.tempDefUser));
 			if (Float.parseFloat(this.temp) > Float.parseFloat(this.tempDefUser)) {
 				this.controlGpioExample.onLedone();
-				changed();
+				changed(); //notificar todos os observadores
 				this.ledOne = true;
 			}
+			
 			System.out.println("Umidade padrão: " + Float.parseFloat(this.humiDefUser));
 			if (Float.parseFloat(this.humi) > Float.parseFloat(this.humiDefUser)) {
 				this.controlGpioExample.onLedTwo();
-				changed();
+				changed();//notificar todos os observadores
 				this.ledTwo = true;
 			}
 		}
